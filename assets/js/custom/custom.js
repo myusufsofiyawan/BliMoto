2fe87591142094399baef72640673d3a17587400
agungsapp <agung.dni19@gmail.com>
2023-09-14 09:37:45 +0700

    oke

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -37,26 +37,6 @@ $(document).ready(function () {
   // }
 
 
-  // gpt 
-  function updateBubble(input) {
-    const val = input.value;
-    const bubble = document.getElementById('bubble');
-
-    // Konversi nilai ke format mata uang Rupiah
-    const formattedValue = formatToRupiah(val);
-    bubble.textContent = formattedValue;
-
-    // Sorta magic numbers based on size of the native UI thumb
-    const min = input.min ? parseFloat(input.min) : 0;
-    const max = input.max ? parseFloat(input.max) : 100;
-    const newVal = ((val - min) * 100) / (max - min);
-    bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
-  }
-
-  function formatToRupiah(value) {
-    // Gunakan metode Intl.NumberFormat untuk mengonversi nilai menjadi format mata uang Rupiah
-    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(value);
-  }
 
   // gpt 
 
